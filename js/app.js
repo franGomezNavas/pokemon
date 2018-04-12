6bd6629375c220f2fc27aff10437474c06891551
Laura Moya <lauraemepe@gmail.com>
2018-04-12 18:07:38 +0200

    Va el javascript iniciado

--- js/app.js
+++ js/app.js
@@ -1,2 +1,18 @@
 
+var main = $("#main");
+var pokemon;
 
+$.ajax({
+	url: "http://pokeapi.co/api/v2/pokemon/1",
+	type: "GET",
+	dataType: "JSON",
+	success: handleResponse
+})
+function handleResponse(response){
+	pokemon = response;
+	showPokemon(pokemon)
+}
+
+function showPokemon(pokemon){
+	alert(pokemon.name)
+}
